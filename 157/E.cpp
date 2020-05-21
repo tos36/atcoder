ec534eeaeb9b94069666e97cbebc778648f1f7f4
KJ <toshiyuki.kaji36@gmail.com>
2020-05-21 12:50:06 +0900

    add E

--- 157/E.cpp
+++ 157/E.cpp
@@ -1,5 +1,43 @@
 #include <bits/stdc++.h>
 
+using namespace std;
+
 int main(int, char**) {
+	int n;
+	string s;
+	int q;
+	cin >> n >> s >> q;
+	vector<set<int>> alp;
+	alp.assign(26, set<int>());
+
+	for (int i=0; i<n; i++) {
+		char c = s.at(i);
+		alp[s.at(i) - 'a'].insert(i + 1);
+	}
+
+
+	for (int i=0; i<q; i++) {
+		int qu;
+		scanf("%d", &qu);
+		if (qu==1) {
+			int l;
+			char c;
+			scanf("%d %c", &l, &c);
+			char b = s.at(l - 1);
+			alp[b - 'a'].erase(l);
+			s.at(l - 1) = c;
+			alp[c - 'a'].insert(l);
+		} else {
+			int l, r;
+			scanf("%d %d", &l, &r);
+			int cnt = 0;
+			for (int i=0; i<26; i++) {
+				if (alp[i].empty()) continue;
+				auto itr = alp[i].lower_bound(l);
+				if (itr != alp[i].end() && *itr <= r) cnt++;
+			}
+			cout << cnt << endl;
+		}
+	}
 	return 0;
 }
