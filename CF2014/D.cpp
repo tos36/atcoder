b8051ee318de59fcc3eb4500a21042839a3fb109
KJ <toshiyuki.kaji36@gmail.com>
2020-06-09 13:05:12 +0900

    add CF2014

---
+++ CF2014/D.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+const ll MOD = 1000000007;
+const ll INF = 1LL<<60;
+//int INF = numeric_limits<int>::max();
+
+int main(int, char**) {
+	ll a; cin >> a;
+	cout << a << " " << 2 << endl;
+	return 0;
+}
